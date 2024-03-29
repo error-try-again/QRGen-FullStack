dff27db8b6067664cf929767b220ba0025763036
Yane Karov <yane.karov@gmail.com>
2024-03-29 19:20:24 +1000

    Summary:
       - Modified argument handling for setup function

--- installer/util/ops/setup.sh
+++ installer/util/ops/setup.sh
@@ -53,47 +53,107 @@ set -euo pipefail
 #######################################
 setup() {
   # Creates a base directory structure for the project
-  setup_directory_structure "${frontend_dir}" "${backend_directory}" "${certbot_dir}" "${project_logs_dir}"
+  setup_directory_structure \
+  "${frontend_dir}" \
+  "${backend_directory}" \
+  "${certbot_dir}" \
+  "${project_logs_dir}"
 
   # Initialize the rootless Docker environment if it is not already initialized
   initialize_rootless_docker
 
   # TODO: patch this to make it effective - supposed to ensure the port is available for each service
-  ensure_port_is_available "${exposed_nginx_port}" "auto"
+  ensure_port_is_available \
+  "${exposed_nginx_port}" \
+  "auto"
 
   # Specify the installation procedure
-  handle_auto_install "${auto_install_flag}" "${use_letsencrypt}" "${install_profile}" "${project_root_dir}"
+  handle_auto_install \
+  "${auto_install_flag}" \
+  "${use_letsencrypt}" \
+  "${install_profile}" \
+  "${project_root_dir}"
 
   # TODO: fix the following hardcoding
-  local domain
+  local domain sitemap_path robots_path nginx_conf_path frontend_dotenv_file
   domain="localhost"
+  sitemap_path="${project_root_dir}/sitemap.xml"
+  robots_path="${project_root_dir}/robots.txt"
+  nginx_conf_path="${project_root_dir}/nginx.conf"
+  frontend_dotenv_file="${frontend_dir}/.env"
 
   # Generates the sitemap.xml file for the website to be indexed by search engines - ${backend_scheme}://${domain} is used as the origin
-  generate_sitemap "${backend_scheme}://${domain}" "${sitemap_xml}"
+  generate_sitemap \
+  "${backend_scheme}://${domain}" \
+  "${sitemap_path}"
 
   # Generates the robots.txt file for the website to be indexed by search engines
-  generate_robots "${robots_file}"
+  generate_robots \
+  "${robots_file}"
 
   # Generates the nginx mime.types file for the nginx server
-  generate_nginx_mime_types "${nginx_mime_types_file}"
+  generate_nginx_mime_types \
+  "${nginx_mime_types_file}"
 
   # Generates the dotenv responsible for passing variables to the frontend
-  generate_frontend_dotenv "${frontend_dotenv_file}" "${use_google_api_key}"
+  generate_frontend_dotenv \
+  "${frontend_dotenv_file}" \
+  "${use_google_api_key}"
 
   # Generates the backend Dockerfile responsible for building the backend image
-  generate_backend_dockerfile "${backend_dockerfile}" "${backend_submodule_url}" "${node_version}" "${release_branch}" "${express_port}" "${use_ssl_flag}" "${backend_scheme}://${domain}" "${google_maps_api_key}"
+  generate_backend_dockerfile \
+  "${backend_dockerfile}" \
+  "${backend_submodule_url}" \
+  "${node_version}" \
+  "${release_branch}" \
+  "${express_port}" \
+  "${use_ssl_flag}" \
+  "${backend_scheme}://${domain}" \
+  "${google_maps_api_key}"
 
   # Generates the frontend Dockerfile responsible for building the frontend image
-  generate_frontend_dockerfile "${frontend_dockerfile}" "${frontend_submodule_url}" "${node_version}" "${release_branch}" "${use_google_api_key}" "${sitemap_path}" "${robots_path}" "${nginx_conf_path}" "${mime_types_path}" "${nginx_version}"
+  generate_frontend_dockerfile \
+  "${frontend_dockerfile}" \
+  "${frontend_submodule_url}" \
+  "${node_version}" \
+  "${release_branch}" \
+  "${use_google_api_key}" \
+  "${sitemap_path}" \
+  "${robots_path}" \
+  "${nginx_conf_path}" \
+  "${mime_types_path}" \
+  "${nginx_version}"
 
   [[ ${build_certbot_image} == "true" ]] && generate_certbot_dockerfile
 
   # Generates the docker-compose file responsible for orchestrating the services
-  generate_docker_compose "${docker_compose_file}" "${service_to_standard_config_map[@]}"
+  generate_docker_compose \
+  "${docker_compose_file}" \
+  "${service_to_standard_config_map[@]}"
 
   # Generates the nginx configuration file responsible for routing requests to the backend and frontend
-  generate_nginx_configuration "${backend_scheme}" "${diffie_hellman_parameters_file}" "${dns_resolver}" "${nginx_configuration_file}" "${release_branch}" "${timeout}" "${use_gzip_flag}" "${use_hsts}" "${use_letsencrypt}" "${use_ocsp_stapling}" "${use_self_signed_certs}" "${use_tls_12_flag}" "${use_tls_13_flag}" "${service_to_standard_config_map[@]}"
+  generate_nginx_configuration \
+  "${backend_scheme}" \
+  "${diffie_hellman_parameters_file}" \
+  "${dns_resolver}" \
+  "${nginx_configuration_file}" \
+  "${release_branch}" \
+  "${timeout}" \
+  "${use_gzip_flag}" \
+  "${use_hsts}" \
+  "${use_letsencrypt}" \
+  "${use_ocsp_stapling}" \
+  "${use_self_signed_certs}" \
+  "${use_tls_12_flag}" \
+  "${use_tls_13_flag}" \
+  "${service_to_standard_config_map[@]}"
 
   # Pools the services and builds the images using docker compose
-  build_and_run_docker "${build_certbot_image}" "${docker_compose_file}" "${project_logs_dir}" "${project_root_dir}" "${release_branch}" "${disable_docker_build_caching}"
+  build_and_run_docker \
+  "${build_certbot_image}" \
+  "${docker_compose_file}" \
+  "${project_logs_dir}" \
+  "${project_root_dir}" \
+  "${release_branch}" \
+  "${disable_docker_build_caching}"
 }
